33c21d893da3e7b1af51a7ef6deb6474876d2df6
Clashkid155 <frankdiss9@gmail.com>
2020-06-05 00:20:53 +0100

    Few changes

---
+++ scripts/steam.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+#from __future__ import unicode_literals
+name="Njike Francis"
+HNG_id="HNG-05023"
+email="beluchukwunjike@gmail.com"
+language="Python"
+
+#try:
+outpu = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email beluchukwunjike@gmail.com".format(name,HNG_id,language)
+print(outpu)
+#except:
+#outpu = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(name,HNG_id,language)
+#print(outpu)
