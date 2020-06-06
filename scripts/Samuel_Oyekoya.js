be74fe2fb0b4cb2fd62a39986b708bc02914b153
Clashkid155 <frankdiss9@gmail.com>
2020-06-06 16:18:21 +0100

    Fixed some scripts again

--- scripts/Samuel_Oyekoya.js
+++ scripts/Samuel_Oyekoya.js
@@ -1,6 +1,10 @@
-var email = "samueloyekoya12@gmail.com"
+var email = "samueloyekoya12@gmail.com";
 var name = "Samuel Oyekoya";
 var id = "HNG-01920";
 var language = "javascript";
+<<<<<<< HEAD
 var output = ("Hello world, this is" + " " + name + " " + "with HNGi7 ID" + " " + id + " " + "using" + " " + language + " " + "for stage 2 task email" + " " + email);
+=======
+var output = ("Hello world, this is" + " " + name + " " + "with HNGi7 ID" + " " + id + " " + "using" + " " + language + " " + "for stage 2 task email " + email);
+>>>>>>> ffe62c0... Fixed some scripts again
 console.log(output);
--- scripts/python_HNG-02864.py
+++ scripts/python_HNG-02864.py
@@ -1,4 +1,4 @@
-﻿﻿State1= "Hello World,"
+﻿State1= "Hello World,"
 State2="This is Aduloju with HNGi7 ID HNG-02684"
 State3="using Python for Stage2 task"
 State4="e-mail adulojuoluwatobiloba@gmail.com"
